a8e2eade30925cc7c27f987cbde5267bd36bf707
Sebastian Davrieux <s.davrieux@amsterdam.nl>
2025-01-31 11:48:33 +0100

    Added starting project

---
+++ Hackaton-OOR/Hackaton-OOR/Hackaton_OORApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hackaton_OORApp.swift
+//  Hackaton-OOR
+//
+//  Created by Sebastian Davrieux on 31/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Hackaton_OORApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
